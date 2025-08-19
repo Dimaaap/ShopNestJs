fadc2cf7f1ae075b5583250203fb4014e5d92f25
Dimaaap <procdima49@gmail.com>
2025-08-19 11:11:19 +0300

    Add auth controllers

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,22 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import { UserService } from 'src/user/user.service';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwt: JwtService, private userService: UserService, private prisma: PrismaService){
+
+    EXPIRE_DAY_REFRESH_TOKEN = 1
+    REFRESH_TOKEN_NAME = "refreshToken"
+
+    constructor(private jwt: JwtService, 
+        private userService: UserService, 
+        private prisma: PrismaService,
+        private configService: ConfigService
+    ){
 
     }
 
@@ -26,6 +36,19 @@ export class AuthService {
         const tokens = this.issueTokens(user.id)
 
         return {user, ...tokens}
+    }
+
+    async getNewTokens(refreshToken: string){
+        const result = await this.jwt.verifyAsync(refreshToken)
+
+        if(!result) throw new UnauthorizedException("Invalid refresh token")
+
+        const user = await this.userService.getById(result.id)
+
+        if(user){
+            const tokens = this.issueTokens(user.id)
+            return {user, ...tokens}    
+        }
     }
 
     issueTokens(userId: string){
@@ -48,5 +71,50 @@ export class AuthService {
         if(!user) throw new NotFoundException("User is not found")
 
         return user
+    }
+
+    async validateOAuthLogin(req: any){
+        let user = await this.userService.getByEmail(req.user.email)
+
+        if(!user){
+            user = await this.prisma.user.create({
+                data: {
+                    email: req.user.email,
+                    name: req.user.name,
+                    picture: req.user.profile
+                },
+                include: {
+                    stores: true,
+                    favorites: true,
+                    orders: true
+                }
+            })
+        }
+
+        const tokens = this.issueTokens(user.id)
+        return { user, ...tokens }
+    }
+
+    addRefreshTokenToResponse(res: Response, refreshToken: string) {
+        const expiresIn = new Date()
+        expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+        res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+            httpOnly: true,
+            domain: this.configService.get('SERVER_DOMAIN'),
+            expires: expiresIn,
+            secure: true,
+            sameSite: "none"
+        })
+    }
+
+    removeRefreshTokenFromResponse(res: Response) {
+        res.cookie(this.REFRESH_TOKEN_NAME, "", {
+            httpOnly: true,
+            domain: this.configService.get('SERVER_DOMAIN'),
+            expires: new Date(0),
+            secure: true,
+            sameSite: "none"
+        })
     }
 }
